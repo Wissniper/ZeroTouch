4bbef20236dff71a58b4069ebcc23d924e58f716
Wissniper <wisdom272006@gmail.com>
2026-02-24 22:48:38 +0100

    opencv working

--- src/core/tracker.py
+++ src/core/tracker.py
@@ -1,1 +1,30 @@
 # MediaPipe wrappers
+import cv2 as cv
+import mediapipe as mp
+from mp.tasks import python
+from mp.tasks.python import vision
+
+
+cap = cv.VideoCapture(0)
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv.CAP_PROP_FPS, 30)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+    cv.flip(frame, 1, frame)  # Flip horizontally for a mirror effect
+
+    # BGRA to RGB
+    # cv.cvtColor(frame, cv.COLOR_BGR2RGB, frame)
+
+
+    cv.imshow('Webcam Feed', frame)
+    
+    if cv.waitKey(1) & 0xFF == 27:  # Press 'ESC' to quit
+        break
+
+cap.release()
+cv.destroyAllWindows()
